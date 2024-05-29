3c0567e143d3579e1d9d135b23be3d5a889de771
Cristobal <quilimacox1@gmail.com>
2024-05-29 00:57:07 -0500

    Se crea variable de user

--- routers/jwt_auth_users.py
+++ routers/jwt_auth_users.py
@@ -48,11 +48,18 @@ def search_user_db(username: str):
         return UserDB(**users_db[username]) 
 
 
-async def auth_user(token: str):
+async def auth_user(token: str = Depends(oauth2)):
+
+    user = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales invalidos",
+            headers={"WWW-Autenticate": "Bearer"})
 
 
 async def current_user(user: User = Depends(auth_user)):
-
     if user.disabled:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
