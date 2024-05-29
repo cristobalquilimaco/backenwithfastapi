3561182016a54ba68a7503446a6a1f10d7be826c
Cristobal <quilimacox1@gmail.com>
2024-05-29 00:53:42 -0500

    Se crea auth

--- routers/jwt_auth_users.py
+++ routers/jwt_auth_users.py
@@ -45,7 +45,21 @@ users_db = {
 
 def search_user_db(username: str):
     if username in users_db:
-        return UserDB(**users_db[username])    
+        return UserDB(**users_db[username]) 
+
+
+async def auth_user(token: str):
+
+
+async def current_user(user: User = Depends(auth_user)):
+
+    if user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Usuario no inactivo",
+            headers={"WWW-Autenticate": "Bearer"})
+
+    return user
 
 
 @app.post("/login")
@@ -64,3 +78,6 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
 
     return {"access_token": jwt.encode(access_token, SECRET, algorithm=ALGORITHM), "token_type": "bearer" }
 
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
