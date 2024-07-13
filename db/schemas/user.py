08bfcebf4ca96c617cb79a7bc7355c907818df4a
Cristobal <quilimacox1@gmail.com>
2024-07-13 12:12:54 -0500

    Se agreaga id como string

--- db/schemas/user.py
+++ db/schemas/user.py
@@ -1,8 +1,4 @@
 def user_schemas(user) -> dict:
-    return{"id": user["_id"],
-           "username": user["username"],
-           "email": user["email"]}
-
-    _id: str | None
-    username: str
-    email: str
+    return{"id": str(user["_id"]),
+        "username": user["username"],
+        "email": user["email"]}
