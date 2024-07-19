bf66951fed2074815babcb98f8d821b0abc5bb7e
Cristobal <quilimacox1@gmail.com>
2024-07-18 19:56:07 -0500

    Se ajusta codigo para el manejo de post

--- routers/users_db.py
+++ routers/users_db.py
@@ -1,44 +1,38 @@
+# Clase en vídeo: https://youtu.be/_y9qQZXE24A?t=20480
+
 ### Users DB API ###
 
-
-import email
-from hmac import new
 from fastapi import APIRouter, HTTPException, status
-from pydantic import BaseModel
 from db.models.user import User
-from db.schemas.user import user_schemas, users_schema
+from db.schemas.user import users_schema, user_schemas
 from db.client import db_client
 from bson import ObjectId
 
-router = APIRouter(prefix="/userdb",
-                tags=["userdb"],
-                responses={status.HTTP_404_NOT_FOUND: {"message": "No encontrado"}})
-
-users_list = []
+router = APIRouter(prefix="/usersdb",
+                   tags=["usersdb"],
+                   responses={status.HTTP_404_NOT_FOUND: {"message": "No encontrado"}})
 
 
-@router.get("/", response_model=list(User)) 
+@router.get("/", response_model=list[User])
 async def users():
-    return users_schema(db_client.local.users.find())
+    return users_schema(db_client.users.find())
 
 
-#----PATH
-@router.get("/{id}")
+@router.get("/{id}")  # Path
 async def user(id: str):
     return search_user("_id", ObjectId(id))
-    
 
-#----QUERY
-@router.get("/") 
+
+@router.get("/")  # Query
 async def user(id: str):
-    return search_user("_id", id)
+    return search_user("_id", ObjectId(id))
 
 
 @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
 async def user(user: User):
-    if type(search_user("email", user.email )) == User:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="El suario ya existe")
-    
+    if type(search_user("email", user.email)) == User:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="El usuario ya existe")
 
     user_dict = dict(user)
     del user_dict["id"]
@@ -48,44 +42,38 @@ async def user(user: User):
     new_user = user_schemas(db_client.users.find_one({"_id": id}))
 
     return User(**new_user)
-#------ PUT----- Metodo para actualizar un usuario 
 
-@router.put("/") 
+
+@router.put("/", response_model=User)
 async def user(user: User):
 
-    found = False 
+    user_dict = dict(user)
+    del user_dict["id"]
 
-    for index, save__user in enumerate(users_list): 
-        if save__user.id == user.id:  
-            users_list[index] = user
-            found = True
-    
-    if not found: 
-        return {"Error: El usuaro no se puede actualizar"}
-    
+    try:
+        db_client.users.find_one_and_replace(
+            {"_id": ObjectId(user.id)}, user_dict)
+    except:
+        return {"error": "No se ha actualizado el usuario"}
 
-#-------------------DELETE-------   
-@router.delete("/{id}")
-async def delete_user(id: int): 
-    found = False      
+    return search_user("_id", ObjectId(user.id))
 
-    for index, user in enumerate(users_list): 
-        if user.id == id:  
-            del users_list[index] 
-            found = True
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def user(id: str):
+
+    found = db_client.users.find_one_and_delete({"_id": ObjectId(id)})
 
     if not found:
-        return {"Error: El usuario no se encontró"}
+        return {"error": "No se ha eliminado el usuario"}
+
+# Helper
 
 
+def search_user(field: str, key):
 
-def search_user(field: str, key:str):
     try:
-        user = db_client.users.find_one({field: str, key:str})
+        user = db_client.users.find_one({field: key})
         return User(**user_schemas(user))
     except:
-        return {"Usuario duplicado"}
-    
-
-def search_user(id: int):
-    return ""
+        return {"error": "No se ha encontrado el usuario"}
