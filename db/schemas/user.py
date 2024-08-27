900bb02a33a706ca2c300e429e6e3ef8d328f8ec
Cristobal <quilimacox1@gmail.com>
2024-08-26 20:23:01 -0500

    se ajusta schemas

--- db/schemas/user.py
+++ db/schemas/user.py
@@ -1,9 +1,11 @@
-def user_schemas(user) -> dict:
-    return {
-        "id": str(user["id"]),
+
+def user_schema(user) -> dict:
+    return{
+        "id": user["id"],
         "username": user["username"],
-        "email": user["email"]
+        "email":user["email"],
+        "password":user["password"]
     }
 
-def users_schema(users) -> list:
-    return [user_schemas(user) for user in users]
+def users_schemas(users) -> list:
+    return [user_schema(user) for user in users]
